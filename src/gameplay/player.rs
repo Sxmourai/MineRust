d86d45ff19de2ef97c0084375cbf4aefe25df891
Sxmourai <samourai_x@outlook.fr>
2023-10-29 17:47:48 +0100

    feature: added camera rotation

--- src/gameplay/player.rs
+++ src/gameplay/player.rs
@@ -1,11 +1,16 @@
-use bevy::{prelude::{Query, Transform, Camera, Res, Input, KeyCode, With, Vec3}, input::keyboard, time::Time};
+use std::sync::Mutex;
+
+use bevy::{prelude::*, input::{keyboard, mouse::MouseMotion}, time::Time, window::{CursorMoved, CursorGrabMode, WindowFocused, PrimaryWindow}};
 
 const SPEED: f32 = 10.0;
+const SENSITIVITY: f32 = 0.002;
 
 pub fn player_movement(
+    mut windows: Query<&mut Window, With<PrimaryWindow>>,
     mut query: Query<&mut Transform, With<Camera>>,
     keys: Res<Input<KeyCode>>,
     time: Res<Time>,
+    mut motion_evr: EventReader<MouseMotion>,
 ) {
     for mut pos in query.iter_mut() {
         if keys.pressed(KeyCode::Z) { // Walk
@@ -25,5 +30,29 @@ pub fn player_movement(
             let right = pos.rotation.mul_vec3(Vec3::new(-1.0, 0.0, 0.0));
             pos.translation += right * SPEED * time.delta_seconds();
         }
+        let window = windows.single_mut();
+        if window.cursor.visible == false {
+            for ev in motion_evr.iter() {
+                pos.rotation = Quat::from_rotation_y(-ev.delta.x * SENSITIVITY)
+                * Quat::from_rotation_x(-ev.delta.y * SENSITIVITY)
+                * pos.rotation;
+
+            }
+        }
+    }
+}
+pub fn cursor_grab_system(
+    mut windows: Query<&mut Window, With<PrimaryWindow>>,
+    btn: Res<Input<MouseButton>>,
+    key: Res<Input<KeyCode>>,
+) {
+    let mut window = windows.single_mut();
+    if btn.just_pressed(MouseButton::Left) {
+        window.cursor.grab_mode = CursorGrabMode::Locked;
+        window.cursor.visible = false;
+    }
+    if key.just_pressed(KeyCode::Escape) {
+        window.cursor.grab_mode = CursorGrabMode::None;
+        window.cursor.visible = true;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use bevy::{prelude::{App, Update}, app::Startup, DefaultPlugins};
 mod setup;
 mod gameplay;
-use gameplay::player::player_movement;
+use gameplay::player::*;
 use setup::setup;
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
     .add_systems(Startup, setup)
     .add_systems(Update, player_movement)
+    .add_systems(Update, cursor_grab_system)
     // .insert_resource(WindowDescriptor {
     //     title: "MineRust".to_string(),
     //     width: 640.0,
